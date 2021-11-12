ab01ea94c4ee9074457f23129f00ba06887bbcc6
sassi <attila.sassi@gmail.com>
2021-11-12 19:02:57 +0100

    kész feladat

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,3 @@
+const setCookie = (str, token = 'token') => document.cookie = `${token}=${str}; expires=${new Date(Date.now() + 15 * 60 * 1000).toUTCString()}`;
 
-
-
-
-
-
-// export default setCookie;
+export default setCookie;
